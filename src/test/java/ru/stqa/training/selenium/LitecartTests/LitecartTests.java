1825f13e2b5085f6d54c7c6d88b0467a3a808467
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-06-16 11:54:04 +0300

    homework_6_was redone

--- src/test/java/ru/stqa/training/selenium/LitecartTests/LitecartTests.java
+++ src/test/java/ru/stqa/training/selenium/LitecartTests/LitecartTests.java
@@ -7,8 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -45,241 +48,29 @@ public class LitecartTests {
         driver.get("http://localhost/litecart/admin/");
         login("admin", "admin");
 
-        //Appearance
-        WebElement appearanceMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Appearence')]"));
-        appearanceMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Template')]")));
+        List<WebElement> leftMenuItems = driver.findElements(By.className("name"));
+        List<String> leftMenuItemNames = getElementNames(leftMenuItems);
 
-        WebElement appearanceSubMenuLogotype = driver.findElement(
-                By.xpath("//*[contains(text(), 'Logotype')]"));
-        appearanceSubMenuLogotype.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Logotype')]")));
+        for (String s1 : leftMenuItemNames) {
+            driver.findElement(By.xpath("//span[text()='" + s1 + "']")).click();
+            assertTrue(driver.findElement(By.tagName("h1")).isDisplayed());
 
-        //Catalog
-        WebElement catalogMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Catalog')]"));
-        catalogMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Catalog')]")));
+            List<WebElement> menuSubItems = driver.findElements(By.cssSelector(("[id^=doc-]")));
+            List<String> leftSubMenuItemNames = getElementNames(menuSubItems);
 
-        WebElement catalogSubMenuProductGroups = driver.findElement(
-                By.xpath("//*[contains(text(), 'Product Groups')]"));
-        catalogSubMenuProductGroups.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Product Groups')]")));
+            for (String s2 : leftSubMenuItemNames) {
+                driver.findElement(By.xpath("//span[text()='" + s2 + "']")).click();
+                assertTrue(driver.findElement(By.tagName("h1")).isDisplayed());
+            }
+        }
+    }
 
-        WebElement catalogSubMenuOptionGroups = driver.findElement(
-                By.xpath("//*[contains(text(), 'Option Groups')]"));
-        catalogSubMenuOptionGroups.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Option Groups')]")));
-
-        WebElement catalogSubMenuManufacturers = driver.findElement(
-                By.xpath("//*[contains(text(), 'Manufacturers')]"));
-        catalogSubMenuManufacturers.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Manufacturers')]")));
-
-        WebElement catalogSubMenuSuppliers = driver.findElement(
-                By.xpath("//*[contains(text(), 'Suppliers')]"));
-        catalogSubMenuSuppliers.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Suppliers')]")));
-
-        WebElement catalogSubMenuDeliveryStatuses = driver.findElement(
-                By.xpath("//*[contains(text(), 'Delivery Statuses')]"));
-        catalogSubMenuDeliveryStatuses.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Delivery Statuses')]")));
-
-        WebElement catalogSubMenuSoldOutStatuses = driver.findElement(
-                By.xpath("//*[contains(text(), 'Sold Out Statuses')]"));
-        catalogSubMenuSoldOutStatuses.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Sold Out Statuses')]")));
-
-        WebElement catalogSubMenuQuantityUnits = driver.findElement(
-                By.xpath("//*[contains(text(), 'Quantity Units')]"));
-        catalogSubMenuQuantityUnits.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Quantity Units')]")));
-
-        WebElement catalogSubMenuCSVImportExport = driver.findElement(
-                By.xpath("//*[contains(text(), 'CSV Import/Export')]"));
-        catalogSubMenuCSVImportExport.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'CSV Import/Export')]")));
-
-        //Countries
-        WebElement countriesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Countries')]"));
-        countriesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Countries')]")));
-
-        //Currencies
-        WebElement currenciesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Currencies')]"));
-        currenciesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Currencies')]")));
-
-        //Customers
-        WebElement customersMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Customers')]"));
-        customersMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Customers')]")));
-
-        WebElement customersSubMenuCSVImportExport = driver.findElement(
-                By.xpath("//*[contains(text(), 'CSV Import/Export')]"));
-        customersSubMenuCSVImportExport.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'CSV Import/Export')]")));
-
-        WebElement customersSubMenuNewsletter = driver.findElement(
-                By.xpath("//*[contains(text(), 'Newsletter')]"));
-        customersSubMenuNewsletter.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Newsletter')]")));
-
-        //Geo Zones
-        WebElement geoZonesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Geo Zones')]"));
-        geoZonesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Geo Zones')]")));
-
-        //Languages
-        WebElement languagesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Languages')]"));
-        languagesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Languages')]")));
-
-        WebElement languagesSubMenuStorageEncoding = driver.findElement(
-                By.xpath("//*[contains(text(), 'Storage Encoding')]"));
-        languagesSubMenuStorageEncoding.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Storage Encoding')]")));
-
-        //Modules
-        WebElement modulesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Modules')]"));
-        modulesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Job Modules')]")));
-
-        WebElement modulesSubMenuCustomer = driver.findElement(
-                By.xpath("//*[text()='Customer']"));
-        modulesSubMenuCustomer.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Customer Modules')]")));
-
-        WebElement modulesSubMenuShipping = driver.findElement(
-                By.xpath("//*[contains(text(), 'Shipping')]"));
-        modulesSubMenuShipping.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Shipping Modules')]")));
-
-        WebElement modulesSubMenuPayment = driver.findElement(
-                By.xpath("//*[contains(text(), 'Payment')]"));
-        modulesSubMenuPayment.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Payment Modules')]")));
-
-        WebElement modulesSubMenuOrderTotal = driver.findElement(
-                By.xpath("//*[contains(text(), 'Order Total')]"));
-        modulesSubMenuOrderTotal.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Order Total Modules')]")));
-
-        WebElement modulesSubMenuOrderSuccess = driver.findElement(
-                By.xpath("//*[contains(text(), 'Order Success')]"));
-        modulesSubMenuOrderSuccess.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Order Success Modules')]")));
-
-        WebElement modulesSubMenuOrderAction = driver.findElement(
-                By.xpath("//*[contains(text(), 'Order Action')]"));
-        modulesSubMenuOrderAction.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Order Action Modules')]")));
-
-        //Orders
-        WebElement ordersMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Orders')]"));
-        ordersMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Orders')]")));
-
-        WebElement ordersSubMenuOrderStatuses = driver.findElement(
-                By.xpath("//*[contains(text(), 'Order Statuses')]"));
-        ordersSubMenuOrderStatuses.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Order Statuses')]")));
-
-        //Pages
-        WebElement pagesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Pages')]"));
-        pagesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Pages')]")));
-
-        //Reports
-        WebElement reportsMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Reports')]"));
-        reportsMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Monthly Sales')]")));
-
-        WebElement reportsSubMenuMostSoldProducts = driver.findElement(
-                By.xpath("//*[contains(text(), 'Most Sold Products')]"));
-        reportsSubMenuMostSoldProducts.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Most Sold Products')]")));
-
-        WebElement reportsSubMenuMostShoppingCustomers = driver.findElement(
-                By.xpath("//*[contains(text(), 'Most Shopping Customers')]"));
-        reportsSubMenuMostShoppingCustomers.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Most Shopping Customers')]")));
-
-        //Settings
-        WebElement settingsMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Settings')]"));
-        settingsMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Settings')]")));
-
-        WebElement settingsSubMenuDefaults = driver.findElement(
-                By.xpath("//*[contains(text(), 'Defaults')]"));
-        settingsSubMenuDefaults.click();
-
-        WebElement settingsSubMenuGeneral = driver.findElement(
-                By.xpath("//*[contains(text(), 'General')]"));
-        settingsSubMenuGeneral.click();
-
-        WebElement settingsSubMenuListings = driver.findElement(
-                By.xpath("//*[contains(text(), 'Listings')]"));
-        settingsSubMenuListings.click();
-
-        WebElement settingsSubMenuImages = driver.findElement(
-                By.xpath("//*[contains(text(), 'Images')]"));
-        settingsSubMenuImages.click();
-
-        WebElement settingsSubMenuCheckout = driver.findElement(
-                By.xpath("//*[contains(text(), 'Checkout')]"));
-        settingsSubMenuCheckout.click();
-
-        WebElement settingsSubMenuAdvanced = driver.findElement(
-                By.xpath("//*[contains(text(), 'Advanced')]"));
-        settingsSubMenuAdvanced.click();
-
-        WebElement settingsSubMenuSecurity = driver.findElement(
-                By.xpath("//*[contains(text(), 'Security')]"));
-        settingsSubMenuSecurity.click();
-
-        //Slides
-        WebElement slidesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Slides')]"));
-        slidesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Slides')]")));
-
-        //Tax
-        WebElement taxMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Tax')]"));
-        taxMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Tax Classes')]")));
-
-        WebElement taxSubMenuRates = driver.findElement(
-                By.xpath("//*[contains(text(), 'Tax Rates')]"));
-        taxSubMenuRates.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Tax Rates')]")));
-
-        //Translations
-        WebElement translationsMenuItem = driver.findElement(
-                By.xpath("//*[contains(text(), 'Translations')]"));
-        translationsMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Search Translations')]")));
-
-        WebElement translationsSubMenuScanFiles = driver.findElement(
-                By.xpath("//*[text()='Scan Files']"));
-        translationsSubMenuScanFiles.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Scan Files For Translations')]")));
-
-        WebElement translationsSubMenuCSVImportExport = driver.findElement(
-                By.xpath("//*[contains(text(), 'CSV Import/Export')]"));
-        translationsSubMenuCSVImportExport.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'CSV Import/Export')]")));
-
-        //Users
-        WebElement usersMenuItem = driver.findElement(
-                By.xpath("//*[contains(text(), 'Users')]"));
-        usersMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Users')]")));
-
-        //vQmods
-        WebElement vQmodsMenuItem = driver.findElement(
-                By.xpath("//*[contains(text(), 'vQmods')]"));
-        vQmodsMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'vQmods')]")));
+    private List<String> getElementNames(List<WebElement> elements) {
+        List<String> elementNames = new ArrayList<String>();
+        for (WebElement e : elements) {
+            elementNames.add(e.getText());
+        }
+        return elementNames;
     }
 
     @Test
@@ -302,20 +93,41 @@ public class LitecartTests {
     }
 
     @Test
-    @DisplayName("Test for exercise 8")
-    public void testCheckingForCountriesAndGeofencesOnCountriesPage() {
+    @DisplayName("Test for exercise 8)")
+    public void testCheckingSortingOfCountriesAndGeofencesOnCountriesPage() {
         driver.get("http://localhost/litecart/admin/");
         login("admin", "admin");
 
-        //Countries
+        //a) checking if countries are in alphabetical order
         WebElement countriesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Countries')]"));
         countriesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Countries')]")));
 
-        //Geo Zones
+        List<WebElement> countryNameElements = driver.findElements(By.xpath(
+                "//*[@class='dataTable']//a[contains(@href, 'edit_country') and not(contains(@title,'Edit'))]"));
+        List<String> countryNames = new ArrayList<>();
+        for (WebElement countryName : countryNameElements) {
+            countryNames.add(countryName.getText());
+        }
+        ArrayList<String> sortedCopyOfCountryNames = new ArrayList<>(countryNames);
+        Collections.sort(sortedCopyOfCountryNames);
+
+        Assertions.assertEquals(countryNames, sortedCopyOfCountryNames);
+
+        //b) checking if zones are in alphabetical order for countries with more than one zone
+        List<WebElement> countryZoneElements = driver.findElements(
+                By.xpath("//*[contains(text(), 'Zones') and not(contains(text(), 'Geo Zones'))]"));
+
+        }
+
+    @Test
+    @DisplayName("Test for exercise 9)")
+    public void testCheckingGeofenceSortingOnGeofencesPage() {
+        driver.get("http://localhost/litecart/admin/");
+        login("admin", "admin");
+
         WebElement geoZonesMenuItem = driver.findElement(By.xpath("//*[contains(text(), 'Geo Zones')]"));
         geoZonesMenuItem.click();
-        assertTrue(isElementPresent(By.xpath("//h1[contains(text(), 'Geo Zones')]")));
+
     }
 
     private void login(String username, String password) {
